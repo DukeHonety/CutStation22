c7910f0b8a56bbc0f555d9c4eddf22bd1c1a4ccd
Duke Honety <mitchellcaleb00@gmail.com>
2022-10-24 05:23:29 +0900

    fix: update cors

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const data = require('./data');
+const cors = require('cors');
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors({
+    origin: '*'
+}));
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("MongoDB connected... Ready to fuck")
 }).catch(err => console.log(err))
